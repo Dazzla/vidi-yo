73f8601a5912f740267255dcd0f3e3e7a0bab979
jspc <james@zero-internet.org.uk>
2016-08-16 09:04:18 +0100

    Auto-export environment variables

--- src/entry_point.sh
+++ src/entry_point.sh
@@ -2,18 +2,34 @@
 
 set -e
 set -x
+set -a
 
 : ${CLUSTER_ID:?}
 
 : ${AMI:=ami-31328842}
+: ${DB_DATABASE:=master}
 : ${DB_PASSWORD:=iamsoVERYsmart}
+: ${DB_USERNAME:=admin}
 : ${DOMAIN:=master.dev.nativ-systems.com}
+: ${ELASTICSEARCH_CLUSTERNAME:=elasticsearch}
 : ${ENV:=t}
 : ${IPCODE:=P386}
+: ${MASTERUSER_PASSWORD:=masteruser}
+: ${MONGO_DATABASE:=flex}
+: ${MONGO_PASSWORD:=PlainTextYo!}
+: ${MONGO_REPLICASET:=''}
+: ${MONGO_USERNAME:=flex_user}
+: ${RABBIT_HOST:=localhost}
+: ${RABBIT_PASSWORD:=flex}
+: ${RABBIT_USERNAME:=flex}
+: ${RABBIT_VHOST:=flex}
 : ${REGION:=eu-west-1}
 : ${ROLE_NAME:=FTMioRole}
 : ${SECURITY_GROUP_NAME:=ESR-Video-Test-VPC-Resources-SG}
+: ${SMTP_REPLAY:=mail.ft.com}
 : ${VPC_NAME:=ESR-Video-Test-VPC}
+
+[ -f /.env ] && source /.env
 
 : ${OOYALA_PASSWORD:?}
 
@@ -37,32 +53,34 @@ cp /keys/config ~/.aws/config
 eval $(/tool/set_vars -v "${VPC_NAME}" -s "${SECURITY_GROUP_NAME}" -r "${ROLE_NAME}")
 
 ansible-playbook -i /ansible_hosts /playbooks/instances.yml --extra-vars " \
+    ami=$AMI \
     clusterid=$CLUSTER_ID \
-    security_group_id=$SECURITY_GROUP_ID \
-    web_security_group_id=$WEB_SECURITY_GROUP_ID \
+    db_database=$DB_DATABASE \
+    db_password=$DB_PASSWORD \
+    db_username=$DB_USERNAME \
+    env=$ENV \
+    ipcode=$IPCODE \
     private_security_group_id=$PRIVATE_SECURITY_GROUP_ID \
-    role=$ROLE_ARN \
-    public_subnet_1=$PUBLIC_SUBNET_0 \
-    public_subnet_2=$PUBLIC_SUBNET_1 \
-    public_subnet_3=$PUBLIC_SUBNET_2 \
     private_subnet_1=$PRIVATE_SUBNET_0 \
     private_subnet_2=$PRIVATE_SUBNET_1 \
     private_subnet_3=$PRIVATE_SUBNET_2 \
-    env=$ENV \
-    ami=$AMI \
+    public_subnet_1=$PUBLIC_SUBNET_0 \
+    public_subnet_2=$PUBLIC_SUBNET_1 \
+    public_subnet_3=$PUBLIC_SUBNET_2 \
     region=$REGION \
-    ipcode=$IPCODE \
-    db_password=$DB_PASSWORD "
+    role=$ROLE_ARN \
+    security_group_id=$SECURITY_GROUP_ID \
+    web_security_group_id=$WEB_SECURITY_GROUP_ID
+"
 
-eval $(/tool/set_vars -m hosts -v "${VPC_NAME}" -c "${CLUSTER_ID}")
+eval $(/tool/set_vars -m hosts -v "${VPC_NAME}" -c "${CLUSTER_ID}" )
 
 ansible-playbook -vvv -i /ansible_hosts /playbooks/mio.yml --extra-vars " \
     clusterid=$CLUSTER_ID \
-    db_host=$DB_HOST \
-    db_password=$DB_PASSWORD \
-    domain=$DOMAIN \
-    storage_nodes=$STORAGE_NODES \
-    master_nodes=$MASTER_NODES \
+    elasticsearch_nodes=$ELASTICSEARCH_NODES \
     mongo_nodes=$MONGO_NODES \
-    ooyala_password=$OOYALA_PASSWORD
+    ooyala_password=$OOYALA_PASSWORD \
+    storage_nodes=$STORAGE_NODES
 "
+
+/tool/configure_consul
