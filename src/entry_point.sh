ac86e04f1318d8229f26cc37020c7513817432b8
jspc <james@zero-internet.org.uk>
2016-08-22 10:18:37 +0100

    Split build

--- src/entry_point.sh
+++ src/entry_point.sh
@@ -29,6 +29,8 @@ set -a
 : ${SMTP_REPLAY:=mail.ft.com}
 : ${VPC_NAME:=ESR-Video-Test-VPC}
 
+: ${MODE:=provision}
+
 [ -f /.env ] && source /.env
 
 : ${OOYALA_PASSWORD:?}
@@ -50,37 +52,41 @@ mkdir -p ~/.aws
 cp /keys/credentials ~/.aws/credentials
 cp /keys/config ~/.aws/config
 
-eval $(/tool/set_vars -v "${VPC_NAME}" -s "${SECURITY_GROUP_NAME}" -r "${ROLE_NAME}")
+if [[ "${MODE}" = 'build' ]]; then
+    eval $(/tool/set_vars -v "${VPC_NAME}" -s "${SECURITY_GROUP_NAME}" -r "${ROLE_NAME}")
 
-ansible-playbook -i /ansible_hosts /playbooks/instances.yml --extra-vars " \
-    ami=$AMI \
-    clusterid=$CLUSTER_ID \
-    db_database=$DB_DATABASE \
-    db_password=$DB_PASSWORD \
-    db_username=$DB_USERNAME \
-    env=$ENV \
-    ipcode=$IPCODE \
-    private_security_group_id=$PRIVATE_SECURITY_GROUP_ID \
-    private_subnet_1=$PRIVATE_SUBNET_0 \
-    private_subnet_2=$PRIVATE_SUBNET_1 \
-    private_subnet_3=$PRIVATE_SUBNET_2 \
-    public_subnet_1=$PUBLIC_SUBNET_0 \
-    public_subnet_2=$PUBLIC_SUBNET_1 \
-    public_subnet_3=$PUBLIC_SUBNET_2 \
-    region=$REGION \
-    role=$ROLE_ARN \
-    security_group_id=$SECURITY_GROUP_ID \
-    web_security_group_id=$WEB_SECURITY_GROUP_ID
-"
+    ansible-playbook -i /ansible_hosts /playbooks/instances.yml --extra-vars " \
+      ami=$AMI \
+      clusterid=$CLUSTER_ID \
+      db_database=$DB_DATABASE \
+      db_password=$DB_PASSWORD \
+      db_username=$DB_USERNAME \
+      env=$ENV \
+      ipcode=$IPCODE \
+      private_security_group_id=$PRIVATE_SECURITY_GROUP_ID \
+      private_subnet_1=$PRIVATE_SUBNET_0 \
+      private_subnet_2=$PRIVATE_SUBNET_1 \
+      private_subnet_3=$PRIVATE_SUBNET_2 \
+      public_subnet_1=$PUBLIC_SUBNET_0 \
+      public_subnet_2=$PUBLIC_SUBNET_1 \
+      public_subnet_3=$PUBLIC_SUBNET_2 \
+      region=$REGION \
+      role=$ROLE_ARN \
+      security_group_id=$SECURITY_GROUP_ID \
+      web_security_group_id=$WEB_SECURITY_GROUP_ID
+    "
 
-eval $(/tool/set_vars -m hosts -v "${VPC_NAME}" -c "${CLUSTER_ID}" )
+    echo -e "Build complete.\nPlease, now, configure DNS and ELBs\n\nlove,\njames\nxoxo"
+elif [[ "${MODE}" = 'provision' ]]; then
+    eval $(/tool/set_vars -m hosts -v "${VPC_NAME}" -c "${CLUSTER_ID}" )
 
-ansible-playbook -vvv -i /ansible_hosts /playbooks/mio.yml --extra-vars " \
-    clusterid=$CLUSTER_ID \
-    elasticsearch_nodes=$ELASTICSEARCH_NODES \
-    mongo_nodes=$MONGO_NODES \
-    ooyala_password=$OOYALA_PASSWORD \
-    storage_nodes=$STORAGE_NODES
-"
+    ansible-playbook -vvv -i /ansible_hosts /playbooks/mio.yml --extra-vars " \
+      clusterid=$CLUSTER_ID \
+      elasticsearch_nodes=$ELASTICSEARCH_NODES \
+      mongo_nodes=$MONGO_NODES \
+      ooyala_password=$OOYALA_PASSWORD \
+      storage_nodes=$STORAGE_NODES
+    "
 
-/tool/configure_consul
+    /tool/configure_consul
+fi
