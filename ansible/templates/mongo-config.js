aad181134077cc1bb081672667c4c0c9ad57cd30
jspc <james@zero-internet.org.uk>
2016-09-05 15:25:19 +0100

    Ensure all shards are present and correct

--- ansible/templates/mongo-config.js
+++ ansible/templates/mongo-config.js
@@ -1,1 +1,7 @@
+{% set shards = mongo_shard_nodes.split(',') %}
+{% for shard in shards %}
+sh.addShard( "{{shard}}")
+{% endfor %}
+
 db.createUser({user:"{{ db_masterusername }}", pwd: "{{db_masterpassword }}", roles: ["userAdminAnyDatabase"] })
+sh.enableSharding("admin")
